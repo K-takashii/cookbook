a040b169838527a8c3e1f7a092f14a0f9f62436f
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2022-01-27 08:51:15 +0000

    Add user

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4a0a5601d41a7c295545b919273f551ead4cac5e3c1922660b2b0fe7e55709f3c731b8b3f2d12379ea0281aab728acd5324382635122a37c72fed0e8ced125a7'
